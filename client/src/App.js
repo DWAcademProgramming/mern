ad05f50750a60b248851adfada5ab4ac9774b77f
David <95488893+DWAcademProgramming@users.noreply.github.com>
2022-10-17 16:51:29 -0500

    Profile information added

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import ProfileForm from './components/profile-forms/ProfileForm'; 
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 //Redux
 import { Provider } from 'react-redux'; 
 import store from './store'; 
@@ -39,6 +40,7 @@ const App = () => {
             <Route path="/" element={<Landing />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="profiles" element={<Profiles />} />
             <Route
               path="dashboard"
               element={<PrivateRoute component={Dashboard} />}
